46e82dae972e64415dbb574a0f0c3d2a11d3c5c3
DasLub12 <Daslubster@gmail.com>
2026-06-15 00:06:11 -0400

    Create StopGravClient.java

---
+++ src/main/java/com/stopgrav/StopGravClient.java
@@ -0,0 +1,9 @@
+package com.stopgrav;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StopGravClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
